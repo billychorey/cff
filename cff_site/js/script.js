6979bae6549ce17d5cae6670a9f6dbf839a3e674
william Chorey <williamchorey@williams-MacBook-Pro.local>
2015-10-10 21:01:18 -0400

    saturday 10/10 updates, mobile and most of tablet updated

--- cff_site/js/script.js
+++ cff_site/js/script.js
@@ -4,15 +4,24 @@
 
     $( function(){ //ready function
 
+
         var list = $( ".navigation" ).find( "ul" );
+
+        list.hide();
 
         $( ".mobile_nav_button" ).on( "click", function( e ){
 
-            list.slideToggle();
+            list.slideToggle( 300 );
             e.preventDefault();
         } );
 
+        $( window ).on( "resize load", function( e ){
 
+        	if( $( window ).width() >= 700 ){
+
+        		list.removeAttr( "style" );
+        	}
+        } );
 
     } ); //close of ready function
 
